4fb1dc975ecaafc9c93714dc7e5a302de777f9c1
Viktor Mironiuk <qa.viktormironiuk@gmail.com>
2025-07-08 14:58:36 +0300

    fix: disable mocha parallel mode to prevent Chrome user-data-dir conflict in CI

--- wdio.conf.js
+++ wdio.conf.js
@@ -132,7 +132,8 @@ exports.config = {
     // See the full list at http://mochajs.org/
     mochaOpts: {
         ui: 'bdd',
-        timeout: 60000
+        timeout: 60000,
+        parallel: false
     },
 
     //
